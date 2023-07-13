9926bf6e6b8417a73c6216882abb99660341da4a
Tommasov <tommasovietina@hotmail.com>
2023-07-13 11:57:24 +0200

    First commit

---
+++ php-custom-functions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name:       PHP - Custom Functions
+ * Plugin URI:        https://github.com/Tommasov/php_c_f
+ * Description:       Inserisci codice PHP nel tuo sito, indipendente dal tema
+ * Version:           1.1
+ * Author:            Tommaso Vietina
+ * Author URI:        https://github.com/Tommasov/
+ * Text Domain:       php_c_f
+ */
+
+ include "functions.php";
